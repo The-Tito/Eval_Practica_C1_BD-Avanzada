1bb424ac3e073376323223986799752066df4387
lsprz2773 <243757@ids.upchiapas.edu.mx>
2026-02-05 12:57:22 -0600

    views.sql file created

---
+++ views.sql
@@ -0,0 +1,10 @@
+
+
+
+-- VIEW 3
+-- Nombre del programa, presupuesto anual total, gasto acumulado, porcentaje del presupuesto gastado,
+-- presupuesto restante y CASE con 3 opciones: en curso, cerca del limite y sobrepasado
+
+SELECT
+    p.nombre AS programa,
+    p.presupuesto_anual AS presupuesto total,
